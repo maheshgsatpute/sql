6cfb09ffe225261a936eed34a995e11f2a402045
maheshgsatpute <maheshgsatpute@gmail.com>
2026-04-09 22:23:27 +0530

    add new file

---
+++ 2.day2.sql
@@ -0,0 +1,42 @@
+-- mysql -u root -p
+
+-- create database
+
+create database demo730Apr ;
+show databases;
+
+use demo730Apr;     -- to go into database
+
+select database();   -- to know in which database
+
+-- varchar(limit),int
+create table student(
+name varchar(20),
+email varchar(20),
+phoneno int
+);
+
+show tables;
+describe students;
+desc student;
+
+create table employee(name varchar(20), location varchar(10));
+
+-- show tables;
+
+drop table employee;
+
+-- show tables;
+
+drop database demo730Apr;
+
+show databases;
+
+
+
+
+
+
+
+
+
